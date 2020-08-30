2e4fd4777d83a2ca753c93a06fc44cfd3a24721c
mendax02 <siddharth.rawat@outlook.com>
2020-08-30 20:01:39 +0530

    Implementing MediatR pattern and AutoMappers and Query and Commands

---
+++ src/Ordering/Ordering.Application/Handlers/CheckOutOrderHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Ordering.Application.Commands;
+using Ordering.Application.Mapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Handlers
+{
+    public class CheckOutOrderHandler : IRequestHandler<CheckOutOrderCommand, OrderResponse>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public CheckOutOrderHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<OrderResponse> Handle(CheckOutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = OrderMapper.Mapper.Map<Order>(request);
+            if (orderEntity == null)
+            {
+                throw new ApplicationException("Not mapped");
+            }
+
+            Order newOrder = await _orderRepository.AddAsync(orderEntity);
+
+            var orderResponse = OrderMapper.Mapper.Map<OrderResponse>(newOrder);
+
+            return orderResponse;
+
+
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Application/Queries/GetOrderByUserNameQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Ordering.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Queries
+{
+    public class GetOrderByUserNameQuery :IRequest<IEnumerable<OrderResponse>>
+    {
+        public GetOrderByUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+    }
+}
